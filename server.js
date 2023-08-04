0ce91d0b601f243daf2bc4709e00003ef3f9a860
PrasadKishorDesai <Prasad.desai@WinjitTechnologiesPvtLtd.local>
2023-08-04 18:35:44 +0530

    added docker-compose

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ app.use('/api', studentRoutes);
 app.use('/auth', authRoutes);
 
 app.listen(process.env.PORT, () => {
-    console.log("Server is running on port 8000");
+    console.log("Server is running on port", process.env.PORT);
 });
 
